937f169fdeef7664a75119f39300adb25d9d3705
Sy Rashid <sy.rashid.11@gmail.com>
2021-01-07 23:40:53 -0500

    FEAT: Set up base constructor for step

--- src/js/models/Step.js
+++ src/js/models/Step.js
@@ -1,4 +1,10 @@
 export default class Step {
-    constructor() {
+    constructor(text, duration, prev, next, animation_url, active) {
+      this._text = text;
+      this._duration = duration;
+      this._prev = prev;
+      this._next = next;
+      this._animation_url = animation_url;
+      this._active = active;
     }
 }
