e9aef29a07eb880aa1e8b6fe218f3cc4b6e3956d
Sy Rashid <sy.rashid.11@gmail.com>
2021-01-13 12:43:51 -0500

    FIX: Ignored test coverage checks for 'packages' initNaturalForm and initMusicPlayer

--- src/js/functions/initNaturalForm.js
+++ src/js/functions/initNaturalForm.js
@@ -1,3 +1,4 @@
+/* istanbul ignore file */
 const initNaturalLanguageForm = (formObj) => {
   // show and hide styled inputs, update natural language statement
   $(".input-container").click(function () {
